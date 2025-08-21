61a53e53e41ab82c05a27ffa644f51bb7a9453b0
DuXXuD <duxxud@gmail.com>
2025-08-21 10:38:55 +0200

    add  deposit queue

--- contracts/interfaces/IRWAVault.sol
+++ contracts/interfaces/IRWAVault.sol
@@ -3,27 +3,37 @@ pragma solidity ^0.8.28;
 
 interface IRWAVault {
 
+  struct DepositRequest {
+    address owner;
+    uint256 assets;
+    uint256 fulfilledAssets;
+    bool finished;
+  }
+
   struct RedeemRequest {
     address owner;
     uint256 shares;
-    bool canceled;
-    bool fulfilled;
+    uint256 fulfilledShares;
+    bool finished;
   }
 
-  event RedeemRequested(
-    address indexed controller, 
+  event DepositRequested(
     address indexed owner, 
     uint256 indexed requestId, 
-    address sender, 
     uint256 assets
   );
 
-  error OnlyVaultManager();
-  error OnlyMember();
-  error VaultPaused();
+  event RedeemRequested(
+    address indexed owner, 
+    uint256 indexed requestId, 
+    uint256 shares
+  );
+
   error OnlyRequestOwner();
   error InsufficientBalance();
   error ZeroShares();
   error MaxAssetsExceeded();
   error MustUseRequestRedeem();
+  error AlreadyFinished();
+  error RequestedAssetsExceeded();
 }
