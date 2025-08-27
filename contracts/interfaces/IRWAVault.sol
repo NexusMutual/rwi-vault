4e7d64b6f2479adea4f95ae16eac330773748232
DuXXuD <duxxud@gmail.com>
2025-08-27 12:59:55 +0200

    add proposals for changing base apy

--- contracts/interfaces/IRWAVault.sol
+++ contracts/interfaces/IRWAVault.sol
@@ -17,17 +17,24 @@ interface IRWAVault {
     bool finished;
   }
 
+  struct BaseApyConfig {
+    uint32 apy;
+    uint32 activeFrom; // for new proposal it is in future, updated on actual change
+    uint assetsPerShare; // updated on actual change
+  }
+
   event DepositRequested(
     address indexed owner, 
     uint256 indexed requestId, 
     uint256 assets
   );
-
   event RedeemRequested(
     address indexed owner, 
     uint256 indexed requestId, 
     uint256 shares
   );
+  event BaseApyChangeProposed(uint newBaseApy, uint activeFrom);
+  event BaseApyChangeExecuted(uint newBaseApy, uint activeFrom, uint assetsPerShare);
 
   error OnlyRequestOwner();
   error InsufficientBalance();
@@ -36,4 +43,7 @@ interface IRWAVault {
   error MustUseRequestRedeem();
   error AlreadyFinished();
   error RequestedAssetsExceeded();
+  error ProposalActiveBeforePreviousOne();
+  error ProposalDoesntExist();
+  error ProposalNotActive();
 }
