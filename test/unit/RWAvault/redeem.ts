ce285380dbb26b38b9250c015c60aa1b2e66960b
DuXXuD <duxxud@gmail.com>
2025-10-24 11:10:45 +0200

    feat: redeems can be partially fulfilled

--- test/unit/RWAvault/redeem.ts
+++ test/unit/RWAvault/redeem.ts
@@ -17,6 +17,7 @@ describe('redeem', function () {
   let rwaVault : RWAVault;
   let usdcMock : ERC20UsdcMock;
   let registry : Registry;
+  let user: any;
   let userShares : bigint;
 
   async function setupFixture() {
@@ -39,14 +40,13 @@ describe('redeem', function () {
     ({members, nonMembers, vaultManager} = accounts);
     ({rwaVault, usdcMock, registry} = contracts);
 
-    const user = members[0];
+    user = members[0];
     const depositAmount = parseUsdc("1000");
 
     userShares = await _deposit(user, depositAmount);
   });
 
   it('only members can redeem', async function () {
-    const user = members[0];
     const nonMember = nonMembers[0];
 
     await rwaVault.connect(user).transfer(nonMember.address, userShares);
@@ -57,7 +57,6 @@ describe('redeem', function () {
   });
 
   it('redeem request goes to the queue when submitted', async function () {
-    const user = members[0];
     await _requestRedeem(user, userShares);
 
     expect(await rwaVault.balanceOf(user.address)).to.equal(0);
@@ -69,7 +68,7 @@ describe('redeem', function () {
   });
 
   it('only vault manager can fulfill the request', async function () {
-    await expect(rwaVault.fulfillRedeems(1, userShares)).to.be.revertedWithCustomError(registry, 'ContractDoesNotExist');
+    await expect(rwaVault.fulfillRedeems(1, parseUsdc("1"))).to.be.revertedWithCustomError(registry, 'ContractDoesNotExist');
   });
 
   it('vault manager can fulfill the requests in FIFO order', async function () {
@@ -87,7 +86,6 @@ describe('redeem', function () {
     const userShares3 = await _deposit(user3, depositAmount3);
     await _requestRedeem(user3, userShares3);
 
-    await usdcMock.mint(vaultManager, parseUsdc("10000"));
     await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), parseUsdc("10000"));
     await rwaVault.connect(vaultManager).fulfillRedeems(2, parseUsdc("10000"));
 
@@ -103,35 +101,107 @@ describe('redeem', function () {
   });
 
   it('total distributed assets must be capped by maxTotalAssets', async function () {
-    const user = members[0];
     await _requestRedeem(user, userShares);
 
-    const expectTotalAssets = await rwaVault.convertToAssets(userShares);
+    const maxTotalAssets = parseUsdc("500");
+    const vaultManagerBalanceStart = await usdcMock.balanceOf(vaultManager);
+    const userBalanceStart = await usdcMock.balanceOf(user);
 
-    await usdcMock.mint(vaultManager, parseUsdc("10000"));
+    await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), vaultManagerBalanceStart);
+    await rwaVault.connect(vaultManager).fulfillRedeems(1, maxTotalAssets);
+
+    expect(await usdcMock.balanceOf(vaultManager)).to.equal(vaultManagerBalanceStart - maxTotalAssets);
+    expect(await usdcMock.balanceOf(user)).to.equal(userBalanceStart + maxTotalAssets);
+  });
+
+  it('last fulfilled request can be partially fulfilled', async function () {
+    const user1 = members[0];
+    const userShares1 = await rwaVault.balanceOf(user1.address);
+    await _requestRedeem(user1, userShares1);
+
+    const user2 = members[1];
+    const depositAmount2 = parseUsdc("2000");
+    const userShares2 = await _deposit(user2, depositAmount2);
+    await _requestRedeem(user2, userShares2);
+
+    
+    await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), parseUsdc("2000"));
+    await rwaVault.connect(vaultManager).fulfillRedeems(2, parseUsdc("2000"));
+    const expectedFulfilledShares = await rwaVault.convertToShares(parseUsdc("2000"));
+    
+
+    const [request1, request2] = await rwaVault.getRedeemRequests([1,2,3]);
+    expect(request1.fulfilledShares).to.equal(userShares1);
+    expect(request1.status).to.equal(RequestStatus.FULFILLED);
+    expect(request2.fulfilledShares).to.equal(expectedFulfilledShares - userShares1);
+    expect(request2.status).to.equal(RequestStatus.PENDING);
+  });
+
+  it('should continue fulfilling starting from the last partially fulfilled request', async function () {
+    const user1 = members[0];
+    const userShares1 = await rwaVault.balanceOf(user1.address);
+    await _requestRedeem(user1, userShares1);
+
+    const user2 = members[1];
+    const depositAmount2 = parseUsdc("2000");
+    const userShares2 = await _deposit(user2, depositAmount2);
+    await _requestRedeem(user2, userShares2);
+
+    const user3 = members[2];
+    const depositAmount3 = parseUsdc("3000"); 
+    const userShares3 = await _deposit(user3, depositAmount3);
+    await _requestRedeem(user3, userShares3);
+
     await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), parseUsdc("10000"));
-    await expect(rwaVault.connect(vaultManager).fulfillRedeems(1, expectTotalAssets - 1n))
-            .to.be.revertedWithCustomError(rwaVault, 'MaxAssetsExceeded');
+    await rwaVault.connect(vaultManager).fulfillRedeems(2, parseUsdc("2000"));
+
+    const [request2_1] = await rwaVault.getRedeemRequests([2]);
+    expect(request2_1.status).to.equal(RequestStatus.PENDING);
+
+    await rwaVault.connect(vaultManager).fulfillRedeems(3, parseUsdc("100"));
+
+    const [request2_2] = await rwaVault.getRedeemRequests([2]);
+    expect(request2_2.status).to.equal(RequestStatus.PENDING);
+
+    await rwaVault.connect(vaultManager).fulfillRedeems(3, parseUsdc("1000"));
+    const [request2_3, request3] = await rwaVault.getRedeemRequests([2,3]);
+    expect(request2_3.status).to.equal(RequestStatus.FULFILLED);
+    expect(request2_3.fulfilledShares).to.equal(userShares2);
+    expect(request3.fulfilledShares).to.be.greaterThan(0);
   });
 
   it('totalAssets is updated on fulfillment', async function () {
      // deposit to have more assets in the vault, so difference in totalAssets can be testet
     await _deposit(members[1], parseUsdc("1000"));
     
-    const user = members[0];
     await _requestRedeem(user, userShares);
 
-    await usdcMock.mint(vaultManager, parseUsdc("10000"));
     await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), parseUsdc("10000"));
 
     const userAssetsBefore = await usdcMock.balanceOf(user.address);
-    await networkHelpers.mine(); // mine 1 block to make sure the totalAssets is updated
     const totalAssetsBefore = await rwaVault.totalAssets();
     await rwaVault.connect(vaultManager).fulfillRedeems(1, parseUsdc("10000"));
 
     const totalAssetsAfter = await rwaVault.totalAssets();
     const userAssetsAfter = await usdcMock.balanceOf(user.address);
     expect(totalAssetsBefore - totalAssetsAfter).to.equal(userAssetsAfter - userAssetsBefore);
+  });
+
+  it('user or vault manager can cancel the request', async function () {
+    await _requestRedeem(user, userShares);
+
+    await expect(rwaVault.connect(members[1]).cancelRedeemRequest(1)).to.be.revertedWithCustomError(rwaVault, 'OnlyRequestOwnerOrVaultManager');
+
+    // vault manager should also be able to cancel the request
+    await expect(rwaVault.connect(vaultManager).cancelRedeemRequest.staticCall(1)).to.not.be.revert(ethers);
+
+    expect(await rwaVault.balanceOf(user.address)).to.equal(0);
+    await rwaVault.connect(user).cancelRedeemRequest(1);
+    
+    const [request] = await rwaVault.getRedeemRequests([1]);
+    expect(request.fulfilledShares).to.equal(0);
+    expect(request.status).to.equal(RequestStatus.CANCELLED);
+    expect(await rwaVault.balanceOf(user.address)).to.equal(userShares);
   });
 
   it('cancelled requests are skipped on fulfillment', async function () {
@@ -152,7 +222,6 @@ describe('redeem', function () {
     await rwaVault.connect(user2).cancelRedeemRequest(2);
     
     const expectTotalAssets = await rwaVault.convertToAssets(userShares1 + userShares3);
-    await usdcMock.mint(vaultManager, expectTotalAssets)
     await usdcMock.connect(vaultManager).approve(await rwaVault.getAddress(), expectTotalAssets);
     await rwaVault.connect(vaultManager).fulfillRedeems(3, expectTotalAssets);
 
@@ -165,21 +234,4 @@ describe('redeem', function () {
     expect(request3.status).to.equal(RequestStatus.FULFILLED);
   });
 
-  it('user or vault manager can cancel the request', async function () {
-    const user = members[0];
-    await _requestRedeem(user, userShares);
-
-    await expect(rwaVault.connect(members[1]).cancelRedeemRequest(1)).to.be.revertedWithCustomError(rwaVault, 'OnlyRequestOwnerOrVaultManager');
-
-    // vault manager should also be able to cancel the request
-    await expect(rwaVault.connect(vaultManager).cancelRedeemRequest.staticCall(1)).to.not.be.revert(ethers);
-
-    expect(await rwaVault.balanceOf(user.address)).to.equal(0);
-    await rwaVault.connect(user).cancelRedeemRequest(1);
-    
-    const [request] = await rwaVault.getRedeemRequests([1]);
-    expect(request.fulfilledShares).to.equal(0);
-    expect(request.status).to.equal(RequestStatus.CANCELLED);
-    expect(await rwaVault.balanceOf(user.address)).to.equal(userShares);
-  });
 });
