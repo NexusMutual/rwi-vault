4da063e20c4b987a7b80b46911f32e212f1bcdc1
DuXXuD <duxxud@gmail.com>
2026-03-11 12:36:55 +0100

    fix: test coverage config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,14 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import "@nomicfoundation/hardhat-typechain";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxMochaEthersPlugin],
+  typechain: {
+    outDir: "types/ethers-contracts",
+  },
   solidity: {
     profiles: {
       default: {
@@ -28,6 +32,11 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    default: {
+      type: "edr-simulated",
+      chainType: "l1",
+      allowUnlimitedContractSize: true,
+    },
     hardhatMainnet: {
       type: "edr-simulated",
       chainType: "l1",
@@ -42,12 +51,25 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    tenderlyvnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("TENDERLYVNET_RPC_URL"),
+      accounts: [configVariable("TENDERLYVNET_PRIVATE_KEY")],
+    }
   },
   paths: {
     tests: {
       mocha: "./test"
     },
   },
+  test: {
+    solidity: {
+      fuzz: {
+        runs: process.env.COVERAGE ? 16 : 256,
+      },
+    },
+  }
 };
 
 export default config;
