1418e54a88bd6b2e03abd3a306f57e2fb4776f6f
DuXXuD <duxxud@gmail.com>
2026-03-16 09:48:55 +0100

    fix: hardhat config fork network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import "@nomicfoundation/hardhat-typechain";
+import "@nomicfoundation/hardhat-verify";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
@@ -42,9 +43,13 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("MAINNET_RPC_URL"),
     },
-    hardhatMainnet: {
+    hardhatMainnetFork: {
       type: "edr-simulated",
       chainType: "l1",
+      forking: {
+        enabled: true,
+        url: configVariable("MAINNET_RPC_URL"),
+      }
     },
     hardhatOp: {
       type: "edr-simulated",
@@ -62,6 +67,11 @@ const config: HardhatUserConfig = {
       url: configVariable("TENDERLYVNET_RPC_URL"),
       accounts: [configVariable("TENDERLYVNET_PRIVATE_KEY")],
     }
+  },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+    },
   },
   paths: {
     tests: {
